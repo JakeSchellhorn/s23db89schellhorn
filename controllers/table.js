cc3f4d7b63c0546a6d84a7fd8cc9d67be533bc2a
Jake Schellhorn <j.schellhorn.02@gmail.com>
2023-11-07 09:30:25 -0600

    created a list all webpage

--- controllers/table.js
+++ controllers/table.js
@@ -20,8 +20,20 @@ exports.table_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Table delete DELETE ' + req.params.id);
     };
     // Handle Costume update form on PUT.
-    exports.table_update_put = function(req, res) {
+exports.table_update_put = function(req, res) {
     res.send('NOT IMPLEMENTED: Table update PUT' + req.params.id);
     };
-    
+
+// List of all Tables
+exports.table_list = async function(req, res) {
+try{
+theTable = await Table.find();
+res.send(theTable);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
+
     
