0fd5cd4848d33c48aa00b8741f7126b07bd8fa08
Jake Schellhorn <j.schellhorn.02@gmail.com>
2023-11-07 09:52:23 -0600

    fixed pug view for list all and implemented so everything is working

--- controllers/table.js
+++ controllers/table.js
@@ -10,16 +10,31 @@ exports.table_detail = function(req, res) {
     res.send('NOTIMPLEMENTED: Table detail: ' +req.params.id);
 }
 
-//Handle Table create on POST
-exports.table_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Table create POST');
-}
+// Handle Table create on POST.
+exports.table_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Table();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    document.material = req.body.material;
+    document.shape = req.body.shape;
+    document.numlegs = req.body.numlegs
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    }
 
 // Handle Table delete form on DELETE.
 exports.table_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Table delete DELETE ' + req.params.id);
     };
-    // Handle Costume update form on PUT.
+// Handle Table update form on PUT.
 exports.table_update_put = function(req, res) {
     res.send('NOT IMPLEMENTED: Table update PUT' + req.params.id);
     };
@@ -41,7 +56,7 @@ res.send(`{"error": ${err}}`);
 exports.table_view_all_Page = async function(req, res) {
 try{
 theTable = await Table.find();
-res.render('tables', { title: 'Table Search Results', results: theTable });
+res.render('table', { title: 'Table Search Results', results: theTable });
 }
 catch(err){
 res.status(500);
--- routes/table.js
+++ routes/table.js
@@ -1,14 +1,3 @@
-/*var express = require('express');
-var router = express.Router();
-
- GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('table', { title: 'Search Results Table' });
-});
-
-module.exports = router;
-*/
-
 var express = require('express');
 const table_controlers= require('../controllers/table');
 var router = express.Router();
